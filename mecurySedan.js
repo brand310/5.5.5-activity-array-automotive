222c172b20e73f0794d312c9f356c08b06076691
Brandon Sandoval <105913779+brand310@users.noreply.github.com>
2022-08-18 01:05:09 -0700

    ASSIGNMENTS DONE

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,50 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicle.js")
 
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
+class car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    checkService() {
+        if (this.mileage > 50000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {            
+            console.log("Engine start.");
+            return this.started = true
+        } else {
+            console.log("No fuel.");
+            return this.started = false;
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + "Over Capacity. Not enough room for additional passengers!");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + "Over capacity.");
+        }
+    }
+
+
+}
+
+let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "Red", 10000);
+
 console.log(v.make)
